dc4b87673cbfd736e2d832b62c3244f5a13099ec
prime-infinity <soe.a.prime@gmail.com>
2021-10-07 22:05:36 +0100

    fresh start on header

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,5 +1,30 @@
 <template>
     <div>
         
+        <mainHeader />
+
     </div>
 </template>
+
+<script>
+    import mainHeader from '../components/mainHeader.vue'
+
+    export default {
+        name: 'index',
+          components: {
+            mainHeader,
+
+        },
+        computed:{
+
+            component:function(){
+                return false
+            }
+           
+        },
+        mounted(){
+
+        }
+    }
+
+</script>
