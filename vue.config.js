f6b8d5bddaa2b479c52b96434159ee609f736b3d
prime-infinity <soe.a.prime@gmail.com>
2021-10-15 12:14:01 +0100

    pp

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    publicPath: '/orb/'
-    /*publicPath: process.env.NODE_ENV === 'production'
+    //publicPath: '/orb/'
+    publicPath: process.env.NODE_ENV === 'production'
     ? '/orb/'
-    : '/'*/
+    : '/'
 }
