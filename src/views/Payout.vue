d57308b082b53e18ff6f5a81add701112c471c07
Evans Edosa <edosaosagieevans@gmail.com>
2021-12-18 03:26:05 +0100

    payout try

--- src/views/Payout.vue
+++ src/views/Payout.vue
@@ -1,7 +1,46 @@
 <template>
-    <div class="row pt-4 pt-md-0">
-        <h2>you are in Payout</h2>
+    <div class="">
+
+        <div class="container pt-5">
+            <div class="pt-4 pt-md-0">
+                <h2>Payout Method</h2>
+            </div>
+
+            <div class="float-right">
+                <h6 class="m-0 p-0">Request Payout</h6>
+                <a href="" class="m-0 p-0">jds 973299</a>
+            </div>
+
+        </div>
+
+        <div class="container mt-5">
+            <div class="row offset-md-2 pt-5 gutter">
+                <div class="col-md-6 col-sm-12 card shadow-sm">
+                    <div class="p-3">
+                        <i class="float-right">i</i>
+                        <h5 class="font-weight-bold"> <i> i </i> Zenith Bank</h5>
+                        <p class="text-muted m-0 p-0">****9878</p>
+                        <p class="font-weight-bold m-0 p-0">Osagie Edosa</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-sm-12 card shadow-sm">
+                    <div class="p-3">
+                        <i class="float-right">i</i>
+                        <h5 class="font-weight-bold"> <i> i </i> Zenith Bank</h5>
+                        <p class="text-muted m-0 p-0">****9878</p>
+                        <p class="font-weight-bold m-0 p-0">Osagie Edosa</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- <hr class="divider"> -->
+        
     </div>
+
+    
 </template>
 
 <script>
