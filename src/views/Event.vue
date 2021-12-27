220cf06e85f951ff2974368208bb819df1558b09
Evans Edosa <edosaosagieevans@gmail.com>
2021-12-27 23:24:31 +0100

    evnt view

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -1,6 +1,117 @@
 <template>
-    <div class="row pt-4 pt-md-0">
-        <h2>you are in an event</h2>
+    <div>
+      <div class="container d-flex mt-5">
+            <div class="mr-auto">
+                <h3 class="m-0 font-weight-bold">Manage Event</h3>
+                <p class="m-0">Table showing all payout activities.</p>
+            </div>
+
+            <div class="ml-auto">
+                <a href="" class="btn btn-dark btn-block text-white">
+                    <p class="m-0 text-muted">Create Event</p>
+                </a>
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <div class="row no-gutters"> 
+                <div class="col-md-5">
+                    <input type="text" class="form-control event-form-input">
+                </div>
+                <div class="col-md-5">
+                    <input type="text" class="form-control event-form-input">
+                </div>
+                <div class="col-md-2">
+                    <button class="event-form-button btn-block">Search events</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6 p-3 event-card">
+                    <div class="row">
+                        <div class="col-2 text-right">
+                            <p class="m-0 p-0">SEPT</p>
+                            <h5 class="font-weight-bold m-0 p-0 date-text">21</h5>
+                            <p class="m-0 p-0 font-weight-bold">FRI</p>
+                            <p class="m-0 p-0 text-muted">0730</p>
+                        </div>
+                        <div class="col-10">
+                            <h4 class="font-weight-bold m-0 p-0">Wizkid Live in Lagos</h4>
+                            <p class="m-0 p-0">Location Address</p>
+                            <p class="m-0 p-0 text-muted">Art & Entertainment</p>
+                            <p class="m-0 p-0 small">Unpublished</p>
+                        </div>
+                    </div>
+                    
+                        <span class="float-right" style="margin-top:-24px;">
+                            <i>jhscsjh</i>
+                        </span>
+                </div>
+                  <div class="col-md-6 p-3 event-card">
+                    <div class="row">
+                        <div class="col-2 text-right">
+                            <p class="m-0 p-0">SEPT</p>
+                            <h5 class="font-weight-bold m-0 p-0 date-text">21</h5>
+                            <p class="m-0 p-0 font-weight-bold">FRI</p>
+                            <p class="m-0 p-0 text-muted">0730</p>
+                        </div>
+                        <div class="col-10">
+                            <h4 class="font-weight-bold m-0 p-0">Wizkid Live in Lagos</h4>
+                            <p class="m-0 p-0">Location Address</p>
+                            <p class="m-0 p-0 text-muted">Art & Entertainment</p>
+                            <p class="m-0 p-0 small">Unpublished</p>
+                        </div>
+                    </div>
+                    
+                        <span class="float-right" style="margin-top:-24px;">
+                            <i>jhscsjh</i>
+                        </span>
+                </div>
+                  <div class="col-md-6 p-3 event-card">
+                    <div class="row">
+                        <div class="col-2 text-right">
+                            <p class="m-0 p-0">SEPT</p>
+                            <h5 class="font-weight-bold m-0 p-0 date-text">21</h5>
+                            <p class="m-0 p-0 font-weight-bold">FRI</p>
+                            <p class="m-0 p-0 text-muted">0730</p>
+                        </div>
+                        <div class="col-10">
+                            <h4 class="font-weight-bold m-0 p-0">Wizkid Live in Lagos</h4>
+                            <p class="m-0 p-0">Location Address</p>
+                            <p class="m-0 p-0 text-muted">Art & Entertainment</p>
+                            <p class="m-0 p-0 small">Unpublished</p>
+                        </div>
+                    </div>
+                    
+                        <span class="float-right" style="margin-top:-24px;">
+                            <i>jhscsjh</i>
+                        </span>
+                </div>
+                  <div class="col-md-6 p-3 event-card">
+                    <div class="row">
+                        <div class="col-2 text-right">
+                            <p class="m-0 p-0">SEPT</p>
+                            <h5 class="font-weight-bold m-0 p-0 date-text">21</h5>
+                            <p class="m-0 p-0 font-weight-bold">FRI</p>
+                            <p class="m-0 p-0 text-muted">0730</p>
+                        </div>
+                        <div class="col-10">
+                            <h4 class="font-weight-bold m-0 p-0">Wizkid Live in Lagos</h4>
+                            <p class="m-0 p-0">Location Address</p>
+                            <p class="m-0 p-0 text-muted">Art & Entertainment</p>
+                            <p class="m-0 p-0 small">Unpublished</p>
+                        </div>
+                    </div>
+                    
+                        <span class="float-right" style="margin-top:-24px;">
+                            <i>jhscsjh</i>
+                        </span>
+                </div>
+            </div>
+            
+        </div>
     </div>
 </template>
 
@@ -21,3 +132,26 @@
         },  
     }
 </script>
+
+<style scoped>
+body{
+    background: red !important;;
+}
+.event-form-input{
+    height: 56px;
+    border-radius: 0px !important;
+}
+.event-form-button{
+    height: 56px;
+    border-radius: 0px !important;
+}
+.event-card:hover{
+    border-top: solid 1px lightgray;
+    border-bottom: solid 1px lightgray;
+    background-color: #f6f6f6;
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075) !important;
+}
+.date-text{
+    color:#FCA311;
+}
+</style>
--- src/views/EventSearch.vue
+++ src/views/EventSearch.vue
@@ -1,6 +1,277 @@
 <template>
-    <div class="row pt-4 pt-md-0">
-        <h2>you are in an event search</h2>
+    <div>
+        <header class="header p-3">
+            <div class="container">
+                <div class="row justify-content-center py-4">
+                    <div class="col-md-8">
+
+                        <div class="row">
+                            <div class="col-9">
+                                <input type="text" class="form-control ">
+                            </div>
+                            <div class="col-3">
+                                <button class="search-button btn btn-block btn-warning d-none d-md-block"> Search Events </button>
+
+                                <button class="search-button btn btn-block btn-warning d-block d-md-none"> <i class="la las-search"></i></button>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+        <div class="container mt-5">
+            <!-- Nav List -->
+            <div>
+                <span class="nav-list active">All</span>
+                <span class="nav-list">New</span>
+                <span class="nav-list">Art & Culture</span>
+                <span class="nav-list">Entertainment</span>
+                <span class="nav-list">Charity & Fundraiser</span>
+                <span class="nav-list">Politics</span>
+            </div>
+            <!-- Events -->
+            <div class="row mt-5">
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="organiser-info p-2">
+                            <div class="d-flex">
+                                <span>
+                                    <img src="" alt="" class="organiser-img">
+                                </span>
+                                <span>
+                                    <h6 class="small font-weight-bold m-0">Davido</h6>
+                                    <p class="small m-0">2020 Followers</p>
+                                </span>
+
+                            </div>
+                        </div>
+                        <div class="event-image">
+                            <img src="" alt="">
+                        </div>
+                        
+                        <div class="event-info container">
+
+                            <div class="event-date shadow-sm pl-2 pt-1">
+                                <p class="p-0">24</p>
+                                <h5 class="p-0 font-weight-bold yellow-color" style="margin-top:-24px;">Oct</h5>
+                                <p class="p-0 font-weight-bold text-muted" style="margin-top:-16px;">Sat</p>
+                            </div>
+
+                            <span class="d-flex mt-2">
+                                <span class="mr-auto"> 
+                                    <p class="event-tag">Art & Culture</p>
+                                </span>
+
+                                <span class="ml-auto">
+                                    <p class="font-weight-bold event-price small">Free</p>
+                                </span>
+                            </span>
+
+                            <div class="event-text">
+                                <h5>Single and Mingle Life Party Thor and Dr Sheldon</h5>
+                            </div>
+
+
+                            <p><span class="yellow-color">Eastwood </span><span></span>Lagos Nigeria</p>
+
+
+                            <span class="d-flex">
+                                <!-- <span class="mr-auto">Art & Culture</span> -->
+                                <span class="mr-auto">
+                                    <button class="btn btn-warning"> <i class="la las-share">t</i> </button>
+                                </span>
+                                
+                                <span class="ml-auto">
+                                    <button class="btn btn-warning"> <i class="la las-love"></i> Save </button>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="organiser-info p-2">
+                            <div class="d-flex">
+                                <span>
+                                    <img src="" alt="" class="organiser-img">
+                                </span>
+                                <span>
+                                    <h6 class="small font-weight-bold m-0">Davido</h6>
+                                    <p class="small m-0">2020 Followers</p>
+                                </span>
+
+                            </div>
+                        </div>
+                        <div class="event-image">
+                            <img src="" alt="">
+                        </div>
+                        
+                        <div class="event-info container">
+
+                            <div class="event-date shadow-sm pl-2 pt-1">
+                                <p class="p-0">24</p>
+                                <h5 class="p-0 font-weight-bold yellow-color" style="margin-top:-24px;">Oct</h5>
+                                <p class="p-0 font-weight-bold text-muted" style="margin-top:-16px;">Sat</p>
+                            </div>
+
+                            <span class="d-flex mt-2">
+                                <span class="mr-auto"> 
+                                    <p class="event-tag">Art & Culture</p>
+                                </span>
+
+                                <span class="ml-auto">
+                                    <p class="font-weight-bold event-price small">Free</p>
+                                </span>
+                            </span>
+
+                            <div class="event-text">
+                                <h5>Single and Mingle Life Party Thor and Dr Sheldon</h5>
+                            </div>
+
+
+                            <p><span class="yellow-color">Eastwood </span><span></span>Lagos Nigeria</p>
+
+
+                            <span class="d-flex">
+                                <!-- <span class="mr-auto">Art & Culture</span> -->
+                                <span class="mr-auto">
+                                    <button class="btn btn-warning"> <i class="la las-share">t</i> </button>
+                                </span>
+                                
+                                <span class="ml-auto">
+                                    <button class="btn btn-warning"> <i class="la las-love"></i> Save </button>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="organiser-info p-2">
+                            <div class="d-flex">
+                                <span>
+                                    <img src="" alt="" class="organiser-img">
+                                </span>
+                                <span>
+                                    <h6 class="small font-weight-bold m-0">Davido</h6>
+                                    <p class="small m-0">2020 Followers</p>
+                                </span>
+
+                            </div>
+                        </div>
+                        <div class="event-image">
+                            <img src="" alt="">
+                        </div>
+                        
+                        <div class="event-info container">
+
+                            <div class="event-date shadow-sm pl-2 pt-1">
+                                <p class="p-0">24</p>
+                                <h5 class="p-0 font-weight-bold yellow-color" style="margin-top:-24px;">Oct</h5>
+                                <p class="p-0 font-weight-bold text-muted" style="margin-top:-16px;">Sat</p>
+                            </div>
+
+                            <span class="d-flex mt-2">
+                                <span class="mr-auto"> 
+                                    <p class="event-tag">Art & Culture</p>
+                                </span>
+
+                                <span class="ml-auto">
+                                    <p class="font-weight-bold event-price small">Free</p>
+                                </span>
+                            </span>
+
+                            <div class="event-text">
+                                <h5>Single and Mingle Life Party Thor and Dr Sheldon</h5>
+                            </div>
+
+
+                            <p><span class="yellow-color">Eastwood </span><span></span>Lagos Nigeria</p>
+
+
+                            <span class="d-flex">
+                                <!-- <span class="mr-auto">Art & Culture</span> -->
+                                <span class="mr-auto">
+                                    <button class="btn btn-warning"> <i class="la las-share">t</i> </button>
+                                </span>
+                                
+                                <span class="ml-auto">
+                                    <button class="btn btn-warning"> <i class="la las-love"></i> Save </button>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="card">
+                        <div class="organiser-info p-2">
+                            <div class="d-flex">
+                                <span>
+                                    <img src="" alt="" class="organiser-img">
+                                </span>
+                                <span>
+                                    <h6 class="small font-weight-bold m-0">Davido</h6>
+                                    <p class="small m-0">2020 Followers</p>
+                                </span>
+
+                            </div>
+                        </div>
+                        <div class="event-image">
+                            <img src="" alt="">
+                        </div>
+                        
+                        <div class="event-info container">
+
+                            <div class="event-date shadow-sm pl-2 pt-1">
+                                <p class="p-0">24</p>
+                                <h5 class="p-0 font-weight-bold yellow-color" style="margin-top:-24px;">Oct</h5>
+                                <p class="p-0 font-weight-bold text-muted" style="margin-top:-16px;">Sat</p>
+                            </div>
+
+                            <span class="d-flex mt-2">
+                                <span class="mr-auto"> 
+                                    <p class="event-tag">Art & Culture</p>
+                                </span>
+
+                                <span class="ml-auto">
+                                    <p class="font-weight-bold event-price small">Free</p>
+                                </span>
+                            </span>
+
+                            <div class="event-text">
+                                <h5>Single and Mingle Life Party Thor and Dr Sheldon</h5>
+                            </div>
+
+
+                            <p><span class="yellow-color">Eastwood </span><span></span>Lagos Nigeria</p>
+
+
+                            <span class="d-flex">
+                                <!-- <span class="mr-auto">Art & Culture</span> -->
+                                <span class="mr-auto">
+                                    <button class="btn btn-warning"> <i class="la las-share">t</i> </button>
+                                </span>
+                                
+                                <span class="ml-auto">
+                                    <button class="btn btn-warning"> <i class="la las-love"></i> Save </button>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+           
+        </div>
+        
+        
     </div>
 </template>
 
@@ -21,3 +292,73 @@
         },  
     }
 </script>
+<style scoped>
+    .yellow-color{
+        color:#FCA311;
+    }
+    .organiser-img{
+        width: 32px;
+        height: 32px;
+        object-fit: cover;
+        background-color: #FCA311;
+        margin-right: 4px;
+        border-radius: 8px;
+    }
+    .header{
+        height:160px;
+        background-color: #14213D;
+        margin: auto -15px;
+    }
+    input,.search-button{
+        height: 56px;
+    }
+    .nav-list{
+        padding: 8px 16px;
+        margin: auto 4px;
+        background-color: lightgray;
+        border-radius: 4px;   
+    }
+    .nav-list.active{
+        background-color: #FCA311;
+    }
+
+    /* Event */
+    .event-image{
+        height: 160px;
+        background-color: #14213D;
+    }
+    .event-image img{
+        height: 160px;
+        width:100%;
+        object-fit: cover;
+    }
+    .event-date{
+        font-size: 16px;
+        width:64px;
+        height: 64px;
+        border-radius: 8px;
+        background-color: white;
+        margin-top: -36px;
+    }
+    .event-tag{
+        /* margin-top: 12px; */
+        padding:2px 4px;
+        border-radius: 4px;
+        font-size: 12px;
+        background-color: lightgray;
+    }
+    .event-price{
+        font-style: italic;
+    }
+    .event-text{
+        margin-top: -12px;
+        height: 54px;
+        /* background-color: lightgray; */
+    }
+    .event-text>h5{
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
+</style>
