209de38fb34bb385d817ea0d31e38899306edb49
Evans Edosa <edosaosagieevans@gmail.com>
2021-12-29 02:10:51 +0100

    event view

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -2,7 +2,7 @@
     <div>
         <div class="container mt-5">
             <div class="row mb-5">
-                <div class="col-md-6">
+                <div class="col-md-8">
                     <div class="header-image mb-3">
                         <img src="" alt="">
                     </div>
@@ -11,33 +11,147 @@
 
                 </div>
 
-                <div class="col-md-6 shadow">
-                    <span class="event-tag">Art & Culture</span>
+                <div class="col-md-4">
+
+                    <div class="event-date shadow-sm mt-4 pl-2 pt-1">
+                        <p class="p-0">24</p>
+                        <h5 class="p-0 font-weight-bold yellow-color" style="margin-top:-24px;">Oct</h5>
+                        <p class="p-0 font-weight-bold text-muted" style="margin-top:-16px;">Sat</p>
+                    </div>
+
+                    <!-- <span class="event-tag">Art & Culture</span> -->
                     <h4 class="mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis adipisci distinctio eius aliquam, ex impedit fugiat illo. Aut delectus</h4>
-                    <p  style="margin-top:-8px;" class="text-muted"><span class="yellow-color">Eastwood </span>Lagos Nigeria</p>
+                    <p  style="margin-top:-4px;">
+                        <span class="yellow-color">Davido Worldwide </span>
+                    </p>
+                    <p  style="margin-top:-16px;" class="text-muted small">112 Follows</p>
 
                 </div>
 
-                <!-- <div class="col-md-12">
-                    <div class="other-images d-flex">
-                        <div class="other-image m-1">
-                            <img src="" alt="">
-                        </div>
+            </div>
 
-                         <div class="other-image m-1">
-                            <img src="" alt="">
-                        </div>
+            <hr>
 
-                         <div class="other-image m-1">
-                            <img src="" alt="">
-                        </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <h4>About this Event</h4>
 
-                         <div class="other-image m-1">
-                            <img src="" alt="">
-                        </div>
+                    <p>
+                    There are no limits on who can become a great entrepreneur. You don’t necessarily need a college degree, a bunch of money in the bank to start something that could become the next major success.
+                    <br><br>
+                    However, you do need a strong plan and the drive to see it through.
+                    <br><br>
+                    Business success is not based on luck, it’s based on hard work and determination, once you set your mind at something and you learn how to succeed at it, then you will surely succeed.
+                    <br><br>
+                    Many businesses failed because they don’t have adequate knowledge about Entrepreneurship, it’s not enough to have the perfect idea, it’s essential to learn how to execute your idea with little or no risk and grow your business into becoming a multinational brand.
+                    <br><br>
+                    This Entrepreneurship Course will teach you the key elements required to startup and manage a business to become a household name. </p>
 
+                    <p>
+                        No matter where you are in your business journey. You are most likely to fall into any of the following categories:
+
+                    </p>
+                    <ul>
+                        <li>Have an idea, but you’re not sure how to start up a business.
+                        </li>
+
+                         <li>Have no idea, but know you want to start up a business.
+                        </li>
+
+                         <li>Have already started your business and you need to expand your business.
+                        </li>
+                    </ul>
+
+                    <p>
+                        In whichever category you fall into, you’re at the right place and we look forward to helping you!
+                        
+                        Don’t be the one to live with the regret of not starting, it’s time to take actions.
+
+                        Join this class and become crystal clear with your business idea, turn the idea in your head into a profitable reality, develop your product or service, convert your target audience into ideal customers while you market your business effectively.
+
+                        Learn about different ways to make your business stand out and walk away with a complete business-plan and business profile write up guide!
+
+                        Learning about your startup is the first step to start a new business and this course is here to help you get started in a profitable way.
+                    </p>
+
+
+What you will learn
+
+The Perfect Business Idea
+
+Product Development
+
+Market Research
+
+Business Management
+
+Thinking Outside The Box
+
+Marketing & Sales Essentials
+
+Effective Business Planning
+
+Business Structuring
+
+Fund Raising
+
+Getting Started
+
+Who should attend
+
+Anyone who wants to avoid the risk of making costly mistakes that could squander your entire capital.
+
+Anyone who wants to understand and implement the strategies that will help you achieve your business goals fast.
+
+What you will get
+
+Certification
+
+Training Materials
+
+Worksheets
+
+Refreshments
+
+Date: Sat. Jan. 29th 2022
+
+Time: 10 am – 4 pm
+
+Venue: 4th Floor, Polystar Building by Maruwa B/Stop, 2nd Roundabout, Lekki Phase 1, Lagos.
+
+COURSE FEE: N80,000
+
+Register HERE
+
+Inquiries: Call or WhatsApp: 0913 117 2933 Mobile: 09152371278
+
+
+                </div>
+
+                <div class="col-md-4">
+
+                    <div class="location">
+                        <h5>Location</h5>
+                        <p class="m-0">
+                            Name of Location <br>
+                            B20 Wole Olateju Crescent <br>
+                            Lekki, LA 106104
+                        </p>
+                        <p class="yellow-color m-0">See Location</p>
                     </div>
-                </div> -->
+
+                    <div class="Date mt-4">
+                        <span>
+                             <p class="m-0">
+                            Name of Location <br>
+                            B20 Wole Olateju Crescent <br>
+                            Lekki, LA 106104
+                            </p>
+                            <p class="yellow-color m-0">Add Calender</p>
+                        </span>
+                    </div>
+
+                </div>
             </div>
         </div>
 
@@ -65,10 +179,13 @@
     }
 </script>
 <style scoped>
-    
+    .yellow-color{
+        color:#FCA311;
+    }
+
     .header-image{
         background-color: #14213D;
-        height: 480px;
+        height: 400px;
     }
 
     .other-image{
@@ -85,6 +202,15 @@
     }
 
     /*  */
+     .event-date{
+        font-size: 16px;
+        width:64px;
+        height: 64px;
+        border-radius: 8px;
+        background-color: white;
+        margin-top: -36px;
+    }
+
     .event-tag{
         /* margin-top: 12px; */
         padding:4px 8px;
@@ -92,5 +218,7 @@
         font-size: 12px;
         background-color: #FEE3B8;
     }
+
+    
 
 </style>
